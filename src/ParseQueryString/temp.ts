284fa7e38e81c3cfd5ac10c5e6e30cc6eec58dba
liuying <934725772@qq.com>
2022-12-21 22:04:02 +0800

    分析QueryString 类型体操

--- src/ParseQueryString/temp.ts
+++ src/ParseQueryString/temp.ts
@@ -3,7 +3,7 @@ type MergeParamValue<A extends object, B extends object, K extends keyof A | key
     K extends keyof B ? B[K] : never;
 
 type MergeParam<Params extends object, Other extends object> = {
-    [K in keyof Params | keyof Other]: MergeParamValue<Params, Other, K>
+    [K in keyof (Params & Other)]: MergeParamValue<Params, Other, K>
 }
 
 type KeyValuePair<T extends string> = T extends `${infer K}=${infer V}` ? {
@@ -15,4 +15,39 @@ type ParseQueryString<U extends string> = U extends `${infer param}&${infer Rest
     : KeyValuePair<U>;
 
 
-type a = ParseQueryString<"a=1&b=2&c=3">
+// type a = ParseQueryString<"b=2&c=3&d=4&e=5">
+
+/**
+ *  过程推断分析
+ */
+
+// 1. MergeParam<{b: 2}, ?>
+
+// 2. 拿着剩下的c=3&d=4&e=5继续, MergeParam<{c: 3}, ?>
+
+// 3. 继续d=4&e=5, MergeParam<{d:4}, ?>
+
+// 4. 只剩下了最后一个，这个时候命中KeyValuePair, 也就是{e: 5}
+
+
+/**
+ *   由于上面的步骤下面开始合并， 第3步为MergeParam<{d:4}, {e: 5}> 得到{d: 4, e: 5}
+ *   第2步再把第3步的结果放进去 MergeParam<{c: 3}, {d: 4, e: 5}> 得到 {c: 3, d: 4, e: 5}
+ *   第1步再把第二步解析好的对象放进去，MergeParam<{b: 2}, {c: 3, d: 4, e: 5}>  最终输出 {b:2, c: 3, d: 4, e: 5}
+ */
+
+
+// 以下是自己手动实现的精简版本
+
+type KeyValueMaping<T extends string> = T extends `${infer K}=${infer V}` ? {
+    [P in K]: V
+} : {};
+
+type Reduce<X extends object, Y extends object> = {
+    [K in keyof (X & Y)]: (X & Y)[K]
+}
+
+type URLQueryString<URL extends string> = URL extends `${infer Query}&${infer Rest}` ?
+ Reduce<KeyValueMaping<Query>, URLQueryString<Rest>> : KeyValueMaping<URL>;
+
+ type URL = URLQueryString<"a=1&b=2&c=3">
